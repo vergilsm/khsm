4e7d6eb53a2869cb9b7bd71113b813a1c31238db
vergil <vergil.sm@yandex.ru>
2018-01-03 19:29:10 +0300

    adds tips for user_helpers to spec/model

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -27,6 +27,24 @@ RSpec.describe GameQuestion, type: :model do
     end
   end
 
+    it 'correct .help_hash' do
+      # на фабрике у нас изначально хэш пустой
+      expect(game_question.help_hash).to eq({})
+
+      # добавляем пару ключей
+      game_question.help_hash[:some_key1] = 'blabla1'
+      game_question.help_hash['some_key2'] = 'blabla2'
+
+      # сохраняем модель и ожидаем сохранения хорошего
+      expect(game_question.save).to be_truthy
+
+      # загрузим этот же вопрос из базы для чистоты эксперимента
+      gq = GameQuestion.find(game_question.id)
+
+      # проверяем новые значение хэша
+      expect(gq.help_hash).to eq({some_key1: 'blabla1', 'some_key2' => 'blabla2'})
+    end
+
   # группа тестов на помощь игроку
   context 'user helpers' do
     it 'correct audience_help' do
@@ -38,6 +56,34 @@ RSpec.describe GameQuestion, type: :model do
       # мы не можем знать распределение, но можем проверить хотя бы наличие нужных ключей
       expect(game_question.help_hash).to include(:audience_help)
       expect(game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
+    end
+
+    # проверяем работу 50/50
+    it 'correct fifty_fifty' do
+      # сначала убедимся, в подсказках пока нет нужного ключа
+      expect(game_question.help_hash).not_to include(:fifty_fifty)
+      # вызовем подсказку
+      game_question.add_fifty_fifty
+
+      # проверим создание подсказки
+      expect(game_question.help_hash).to include(:fifty_fifty)
+      ff = game_question.help_hash[:fifty_fifty]
+
+      expect(ff).to include('b') # должен остаться правильный вариант
+      expect(ff.size).to eq 2 # всего должно остаться 2 варианта
+    end
+
+    # проверяем работу звонок другу
+    it 'correct friend_call' do
+      # убедимся что подсказки есть, пока нет нужного ключа
+      expect(game_question.help_hash).not_to include(:friend_call)
+      # вызовем подсказку
+      game_question.add_friend_call
+      # проверим создание подсказки
+      expect(game_question.help_hash).to include(:friend_call)
+
+      fc = game_question.help_hash[:friend_call]
+      expect(fc).to be_a(String)
     end
   end
 end
