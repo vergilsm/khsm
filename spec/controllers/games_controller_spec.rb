5972787663db748edf22aae18533ae1e81d04667
vergil <vergil.sm@yandex.ru>
2018-01-04 15:34:03 +0300

    adds uses fifty_fifty to games_controller_spec

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -164,5 +164,25 @@ RSpec.describe GamesController, type: :controller do
       expect(game.current_game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
       expect(response).to redirect_to(game_path(game))
     end
+
+    # тест на возможность использования подсказки 50х50
+    it 'uses fifty_fifty' do
+      # сперва проверяем что в подсказках текущего вопроса пусто
+      expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
+      expect(game_w_questions.fifty_fifty_used).to be_falsey
+      # запрос в контроллер с нужным типом
+      put :help, id: game_w_questions.id, help_type: :fifty_fifty
+      game = assigns(:game)
+
+      # проверяем, что игра не закончилась, что флажок установился, и подсказка записалась
+      expect(game.finished?).to be_falsey
+      expect(game.fifty_fifty_used).to be_truthy
+      expect(game.current_game_question.help_hash[:fifty_fifty]).to be
+
+      ff = game.current_game_question.help_hash[:fifty_fifty]
+      expect(ff).to include 'd' # правильный ответ 'd'
+      expect(ff.count).to eq 2 # в ответе должно быть два варианта
+      expect(response).to redirect_to(game_path(game))
+    end
   end
 end
